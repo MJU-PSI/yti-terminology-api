7b4ad874c5ef874ece4cb71982737d9eb5557503
Janne Mäki <janne.maki@evident.fi>
2017-03-20 10:26:10 +0200

    Initial

---
+++ src/main/java/fi/csc/termed/search/ElasticSearchService.java
@@ -0,0 +1,9 @@
+package fi.csc.termed.search;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ElasticSearchService {
+    void updateIndex(Notification notification) {
+    }
+}
---
+++ src/main/java/fi/csc/termed/search/Notification.java
@@ -0,0 +1,18 @@
+package fi.csc.termed.search;
+
+public class Notification {
+    Identifier node;
+}
+
+class Identifier {
+    String id;
+}
+
+class Type {
+    String id;
+    Graph graph;
+}
+
+class Graph {
+    String id;
+}
