30dc345c2e0e06499dda14e3aaaccfc30914ed80
Janne Mäki <janne.maki@evident.fi>
2017-05-15 11:23:53 +0300

    Move objects related to notification as static inner classes

--- src/main/java/fi/csc/termed/search/domain/Notification.java
+++ src/main/java/fi/csc/termed/search/domain/Notification.java
@@ -1,11 +1,113 @@
 package fi.csc.termed.search.domain;
 
+import java.util.Date;
+
 public class Notification {
+
+    public static class Graph {
+
+        private String id;
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+    }
+
+    public static class Body {
+
+        private String user;
+        private Date date;
+        private Node node;
+
+        public Node getNode() {
+            return node;
+        }
+
+        public void setNode(Node node) {
+            this.node = node;
+        }
+
+        public String getUser() {
+            return user;
+        }
+
+        public void setUser(String user) {
+            this.user = user;
+        }
+
+        public Date getDate() {
+            return date;
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
+    }
+
+    public static class Node {
+
+        private String id;
+        private Type type;
+
+        public Type getType() {
+            return type;
+        }
+
+        public void setType(Type type) {
+            this.type = type;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+    }
+
+    public static class Type {
+
+        private TypeId id;
+        private Graph graph;
+
+        public Graph getGraph() {
+            return graph;
+        }
+
+        public void setGraph(Graph graph) {
+            this.graph = graph;
+        }
+
+        public TypeId getId() {
+            return id;
+        }
+
+        public void setId(TypeId id) {
+            this.id = id;
+        }
+    }
+
+    public enum TypeId {
+        Term,
+        Concept,
+        TerminologicalVocabulary,
+        Vocabulary
+    }
+
+    public enum EventType {
+        NodeSavedEvent,
+        NodeDeletedEvent,
+        ApplicationReadyEvent,
+        ApplicationShutdownEvent
+    }
 
     private EventType type;
     private Body body;
-
-    public Notification() {}
 
     public EventType getType() {
         return type;
@@ -22,12 +124,4 @@ public class Notification {
     public void setBody(Body body) {
         this.body = body;
     }
-
-    public enum EventType {
-        NodeSavedEvent,
-        NodeDeletedEvent,
-        ApplicationReadyEvent,
-        ApplicationShutdownEvent
-    }
 }
-
