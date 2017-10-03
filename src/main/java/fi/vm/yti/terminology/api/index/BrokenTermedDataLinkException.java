1aa2550b8382d48b2006c045fd48e02687ed8fab
Janne Mäki <janne.maki@evident.fi>
2017-10-03 16:35:33 +0300

    Package protected access since not publicly constructed

--- src/main/java/fi/vm/yti/terminology/api/index/BrokenTermedDataLinkException.java
+++ src/main/java/fi/vm/yti/terminology/api/index/BrokenTermedDataLinkException.java
@@ -1,7 +1,7 @@
 package fi.vm.yti.terminology.api.index;
 
 public class BrokenTermedDataLinkException extends RuntimeException {
-    public BrokenTermedDataLinkException(Vocabulary vocabulary, String nodeId) {
+    BrokenTermedDataLinkException(Vocabulary vocabulary, String nodeId) {
         super("Node (" + nodeId + ") not in graph (" + vocabulary.getGraphId() + ")");
     }
 }
