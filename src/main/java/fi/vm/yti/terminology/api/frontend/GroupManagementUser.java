d15754dc4096ce315103b4f4f810d5f1527992bf
Janne Mäki <janne.maki@evident.fi>
2018-03-07 15:32:48 +0200

    Adapt to group management api changes

--- src/main/java/fi/vm/yti/terminology/api/frontend/GroupManagementUser.java
+++ src/main/java/fi/vm/yti/terminology/api/frontend/GroupManagementUser.java
@@ -1,20 +1,38 @@
 package fi.vm.yti.terminology.api.frontend;
 
+import org.jetbrains.annotations.Nullable;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 public class GroupManagementUser {
 
+    private final UUID id;
     private final String email;
     private final String firstName;
     private final String lastName;
+    @Nullable
+    private final LocalDateTime removalDateTime;
 
     // Jackson constructor
     private GroupManagementUser() {
-        this("", "", "");
+        this(UUID.randomUUID(), "", "", "", null);
     }
 
-    public GroupManagementUser(String email, String firstName, String lastName) {
+    public GroupManagementUser(UUID id,
+                               String email,
+                               String firstName,
+                               String lastName,
+                               @Nullable LocalDateTime removalDateTime) {
+        this.id = id;
         this.email = email;
         this.firstName = firstName;
         this.lastName = lastName;
+        this.removalDateTime = removalDateTime;
+    }
+
+    public UUID getId() {
+        return id;
     }
 
     public String getEmail() {
@@ -27,5 +45,9 @@ public class GroupManagementUser {
 
     public String getLastName() {
         return lastName;
+    }
+
+    public LocalDateTime getRemovalDateTime() {
+        return removalDateTime;
     }
 }
