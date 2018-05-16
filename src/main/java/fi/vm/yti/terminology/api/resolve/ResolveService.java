c427f4f0c974afc69a71cc3f02ab908bb967fc7e
Janne Mäki <janne.maki@evident.fi>
2018-05-16 11:48:17 +0300

    Prefix pattern ends with slash

--- src/main/java/fi/vm/yti/terminology/api/resolve/ResolveService.java
+++ src/main/java/fi/vm/yti/terminology/api/resolve/ResolveService.java
@@ -30,7 +30,7 @@ public class ResolveService {
     private final TermedRequester termedRequester;
     private final String namespaceRoot;
 
-    private static final Pattern PREFIX_PATTERN = Pattern.compile("^(?<prefix>\\w+)$");
+    private static final Pattern PREFIX_PATTERN = Pattern.compile("^(?<prefix>\\w+)/$");
     private static final Pattern PREFIX_AND_RESOURCE_PATTERN = Pattern.compile("^(?<prefix>\\w+)/(?<resource>\\w+)$");
 
     @Autowired
