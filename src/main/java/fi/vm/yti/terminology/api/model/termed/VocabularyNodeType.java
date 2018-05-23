caec777a2d064b04babf135b9d818f879bbc5156
Janne Mäki <janne.maki@evident.fi>
2018-05-23 15:18:55 +0300

    Vocabulary node type to node type mapping

--- src/main/java/fi/vm/yti/terminology/api/model/termed/VocabularyNodeType.java
+++ src/main/java/fi/vm/yti/terminology/api/model/termed/VocabularyNodeType.java
@@ -1,6 +1,17 @@
 package fi.vm.yti.terminology.api.model.termed;
 
 public enum VocabularyNodeType {
-    Vocabulary,
-    TerminologicalVocabulary
+
+    Vocabulary(NodeType.Vocabulary),
+    TerminologicalVocabulary(NodeType.TerminologicalVocabulary);
+
+    private NodeType nodeType;
+
+    VocabularyNodeType(NodeType nodeType) {
+        this.nodeType = nodeType;
+    }
+
+    public NodeType asNodeType() {
+        return this.nodeType;
+    }
 }
