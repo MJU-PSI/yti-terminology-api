a1393982ef499bc1507310257de6107ffedb9b86
Tuomo Soinio <tuomo.soinio@gofore.com>
2019-07-11 09:57:34 +0300

    Fix resolve API to work with both the terminology namespace URI and the longer ...terminological-vocabulary-0 type URI.

--- src/main/java/fi/vm/yti/terminology/api/resolve/ResolveService.java
+++ src/main/java/fi/vm/yti/terminology/api/resolve/ResolveService.java
@@ -1,14 +1,11 @@
 package fi.vm.yti.terminology.api.resolve;
 
-import fi.vm.yti.terminology.api.TermedContentType;
-import fi.vm.yti.terminology.api.TermedRequester;
-import fi.vm.yti.terminology.api.exception.ResourceNotFoundException;
-import fi.vm.yti.terminology.api.exception.VocabularyNotFoundException;
-import fi.vm.yti.terminology.api.model.termed.GenericNode;
-import fi.vm.yti.terminology.api.model.termed.Graph;
-import fi.vm.yti.terminology.api.model.termed.NodeType;
-import fi.vm.yti.terminology.api.resolve.ResolvedResource.Type;
-import fi.vm.yti.terminology.api.util.Parameters;
+import java.util.List;
+import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
@@ -18,12 +15,15 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.UUID;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-
+import fi.vm.yti.terminology.api.TermedContentType;
+import fi.vm.yti.terminology.api.TermedRequester;
+import fi.vm.yti.terminology.api.exception.ResourceNotFoundException;
+import fi.vm.yti.terminology.api.exception.VocabularyNotFoundException;
+import fi.vm.yti.terminology.api.model.termed.GenericNode;
+import fi.vm.yti.terminology.api.model.termed.Graph;
+import fi.vm.yti.terminology.api.model.termed.NodeType;
+import fi.vm.yti.terminology.api.resolve.ResolvedResource.Type;
+import fi.vm.yti.terminology.api.util.Parameters;
 import static java.util.Objects.requireNonNull;
 import static org.springframework.http.HttpMethod.GET;
 
@@ -55,70 +55,58 @@ public class ResolveService {
         String path = uriWithoutParameters.substring(namespaceRoot.length());
 
         Matcher prefixMatcher = PREFIX_PATTERN.matcher(path);
-
         if (prefixMatcher.matches()) {
-
             String prefix = prefixMatcher.group("prefix");
             UUID graphId = findGraphIdForPrefix(prefix);
-
-            return new ResolvedResource(graphId, ResolvedResource.Type.VOCABULARY);
+            return new ResolvedResource(graphId, Type.VOCABULARY);
         }
 
         Matcher prefixAndResourceMatcher = PREFIX_AND_RESOURCE_PATTERN.matcher(path);
-
         if (prefixAndResourceMatcher.matches()) {
-
             String prefix = prefixAndResourceMatcher.group("prefix");
             String resource = prefixAndResourceMatcher.group("resource");
 
             UUID graphId = findGraphIdForPrefix(prefix);
-            UUID conceptId = findConceptId(graphId, resource);
-
-            if (conceptId != null) {
-                return new ResolvedResource(graphId, ResolvedResource.Type.CONCEPT, conceptId);
-            } else {
-                UUID collectionId = findCollectionId(graphId, resource);
-
-                if (collectionId != null) {
-                    return new ResolvedResource(graphId, ResolvedResource.Type.COLLECTION, collectionId);
-                } else {
-                    logger.error("Resource not found URI: " + uri);
-                    throw new ResourceNotFoundException(prefix, resource);
+            List<GenericNode> nodes = findNodes(graphId, resource);
+            if (nodes.size() > 1) {
+                logger.debug("Found " + nodes.size() + " matching nodes for URI: " + uri);
+            }
+            for (GenericNode node : nodes) {
+                switch (node.getType().getId()) {
+                    case TerminologicalVocabulary:
+                        return new ResolvedResource(graphId, Type.VOCABULARY);
+                    case Concept:
+                        return new ResolvedResource(graphId, Type.CONCEPT, node.getId());
+                    case Collection:
+                        return new ResolvedResource(graphId, Type.COLLECTION, node.getId());
+                    default:
+                        logger.debug("Found node of type " + node.getType().getId() + " for URI: " + uri);
                 }
             }
+
+            logger.error("Resource not found URI: " + uri);
+            throw new ResourceNotFoundException(prefix, resource);
         }
 
         logger.error("Unsupported URI: " + uri);
         throw new RuntimeException("Unsupported URI: " + uri);
     }
 
-    private @Nullable UUID findConceptId(UUID graphId, String code) {
-        return findNodeId(graphId, NodeType.Concept, code);
-    }
-
-    private @Nullable UUID findCollectionId(UUID graphId, String code) {
-        return findNodeId(graphId, NodeType.Collection, code);
-    }
-
-    private @Nullable UUID findNodeId(UUID graphId, NodeType nodeType, String code) {
-
+    private List<GenericNode> findNodes(UUID graphId,
+                                        String code) {
         Parameters params = new Parameters();
         params.add("select", "id");
         params.add("select", "type");
         params.add("select", "code");
         params.add("where", "graph.id:" + graphId);
-        params.add("where", "type.id:" + nodeType.name());
         params.add("where", "code:" + code);
         params.add("max", "-1");
 
         List<GenericNode> result =
-                requireNonNull(termedRequester.exchange("/node-trees", GET, params, new ParameterizedTypeReference<List<GenericNode>>() {}));
+            requireNonNull(termedRequester.exchange("/node-trees", GET, params, new ParameterizedTypeReference<List<GenericNode>>() {
+            }));
 
-        if (result.isEmpty()) {
-            return null;
-        } else {
-            return result.get(0).getId();
-        }
+        return result;
     }
 
     // FIXME inefficient implementation but termed doesn't provide better way (afaik)
@@ -127,13 +115,19 @@ public class ResolveService {
         Parameters params = new Parameters();
         params.add("max", "-1");
 
-        return requireNonNull(termedRequester.exchange("/graphs", GET, params, new ParameterizedTypeReference<List<Graph>>() {}))
-                .stream().filter(g -> g.getCode().equalsIgnoreCase(prefix))
-                .findFirst().orElseThrow(() -> new VocabularyNotFoundException(prefix))
-                .getId();
+        return requireNonNull(termedRequester.exchange("/graphs", GET, params, new ParameterizedTypeReference<List<Graph>>() {
+        }))
+            .stream()
+            .filter(g -> g.getCode().equalsIgnoreCase(prefix))
+            .findFirst()
+            .orElseThrow(() -> new VocabularyNotFoundException(prefix))
+            .getId();
     }
 
-    String getResource(@NotNull UUID graphId, @NotNull List<NodeType> types, TermedContentType contentType, @Nullable UUID resourceId) {
+    String getResource(@NotNull UUID graphId,
+                       @NotNull List<NodeType> types,
+                       TermedContentType contentType,
+                       @Nullable UUID resourceId) {
 
         Parameters params = new Parameters();
         params.add("select", "*");
@@ -144,7 +138,9 @@ public class ResolveService {
         return requireNonNull(termedRequester.exchange("/node-trees", GET, params, String.class, contentType));
     }
 
-    private static @NotNull String formatWhereClause(@NotNull UUID graphId, @NotNull List<NodeType> types, @Nullable UUID resourceId) {
+    private static @NotNull String formatWhereClause(@NotNull UUID graphId,
+                                                     @NotNull List<NodeType> types,
+                                                     @Nullable UUID resourceId) {
 
         if (types.size() == 0) {
             throw new IllegalArgumentException("Must include at least one type");
@@ -153,6 +149,6 @@ public class ResolveService {
         String typeClause = types.stream().map(t -> "type.id:" + t.name()).collect(Collectors.joining(" OR "));
 
         return "graph.id:" + graphId +
-                " AND (" + typeClause + ")" + (resourceId != null ? " AND id:" + resourceId : "");
+            " AND (" + typeClause + ")" + (resourceId != null ? " AND id:" + resourceId : "");
     }
 }
