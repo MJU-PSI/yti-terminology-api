fc6a0c442425412d4611f3efe261c7d694bf7e68
Tuomo Soinio <tuomo.soinio@gofore.com>
2019-10-17 13:54:52 +0300

    Add terminology status to concept index.

--- src/main/java/fi/vm/yti/terminology/api/index/Vocabulary.java
+++ src/main/java/fi/vm/yti/terminology/api/index/Vocabulary.java
@@ -1,32 +1,32 @@
 package fi.vm.yti.terminology.api.index;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import fi.vm.yti.terminology.api.util.JsonUtils;
-import org.jetbrains.annotations.NotNull;
-
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+
+import org.jetbrains.annotations.NotNull;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import fi.vm.yti.terminology.api.util.JsonUtils;
 
 public final class Vocabulary {
 
     private final UUID graphId;
     private final String uri;
     private final Map<String, List<String>> label;
+    private final String status;
 
-    // Optional URI for a while,
-    private Vocabulary(@NotNull UUID graphId,  String uri, @NotNull Map<String, List<String>> label) {
+    private Vocabulary(@NotNull UUID graphId,
+                       @NotNull String uri,
+                       @NotNull Map<String, List<String>> label,
+                       @NotNull String status) {
         this.graphId = graphId;
         this.uri = uri;
         this.label = label;
-    }
-
-    private Vocabulary(@NotNull UUID graphId, @NotNull Map<String, List<String>> label) {
-        this.graphId = graphId;
-        this.label = label;
-        this.uri = null;
+        this.status = status;
     }
 
     @NotNull UUID getGraphId() {
@@ -37,26 +37,30 @@ public final class Vocabulary {
         return uri;
     }
 
+    @NotNull String getStatus() {
+        return status;
+    }
+
     static @NotNull Vocabulary createFromExtJson(@NotNull JsonNode json) {
 
         JsonNode typeObj = json.get("type");
         JsonNode properties = json.get("properties");
-        UUID graphId = UUID.fromString(typeObj.get("graph").get("id").textValue());        
-        String uri = null;
-        if(json.get("uri") != null){
-            uri = json.get("uri").textValue();
-        }
+        UUID graphId = UUID.fromString(typeObj.get("graph").get("id").textValue());
+        String uri = json.get("uri").textValue();
         Map<String, List<String>> label = JsonUtils.localizableFromTermedProperties(properties, "prefLabel");
-        return new Vocabulary(graphId, uri, label);
+        String status = JsonUtils.getSinglePropertyValue(properties, "status");
+        return new Vocabulary(graphId, uri, label, status);
     }
 
-    static @NotNull Vocabulary createFromIndex(ObjectMapper mapper, @NotNull JsonNode json) {
+    static @NotNull Vocabulary createFromIndex(ObjectMapper mapper,
+                                               @NotNull JsonNode json) {
 
         UUID graphId = UUID.fromString(json.get("id").textValue());
         String uri = json.get("uri").textValue();
         Map<String, List<String>> label = JsonUtils.jsonToLocalizable(mapper, json.get("label"));
+        String status = json.get("status").textValue();
 
-        return new Vocabulary(graphId, uri, label);
+        return new Vocabulary(graphId, uri, label, status);
     }
 
     @NotNull ObjectNode toElasticSearchObject(ObjectMapper objectMapper) {
@@ -66,6 +70,7 @@ public final class Vocabulary {
         output.set("label", JsonUtils.localizableToJson(objectMapper, label));
         output.put("id", graphId.toString());
         output.put("uri", uri);
+        output.put("status", status);
 
         return output;
     }
