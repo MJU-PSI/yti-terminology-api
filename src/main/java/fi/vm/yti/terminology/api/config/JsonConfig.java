763d9f53290d9a70394dfc5c9dba26a54a5b9ec6
Tuomo Soinio <tuomo.soinio@gofore.com>
2019-10-22 09:27:25 +0300

    Tentative fix (modeled after yti-groupmanagement) for (de)serialization of user removalDateTime.

--- src/main/java/fi/vm/yti/terminology/api/config/JsonConfig.java
+++ src/main/java/fi/vm/yti/terminology/api/config/JsonConfig.java
@@ -1,6 +1,9 @@
 package fi.vm.yti.terminology.api.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +12,8 @@ public class JsonConfig {
 
     @Bean
     ObjectMapper objectMapper() {
-        return new ObjectMapper();
+        return new ObjectMapper()
+            .registerModule(new JavaTimeModule())
+            .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
     }
 }
