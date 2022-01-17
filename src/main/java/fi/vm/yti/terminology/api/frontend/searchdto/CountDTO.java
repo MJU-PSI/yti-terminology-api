e5d1b90da457cfb8d75626201025f7006e8cb8f0
Kalle Kyttälä <kalle.kyttala@rebase.fi>
2022-01-17 12:08:04 +0200

    remove commented code

--- src/main/java/fi/vm/yti/terminology/api/frontend/searchdto/CountDTO.java
+++ src/main/java/fi/vm/yti/terminology/api/frontend/searchdto/CountDTO.java
@@ -23,64 +23,6 @@ public class CountDTO {
         }
     }
 
-    /*
-    class CategoriesCountDTO {
-        private long terminologicalVocabulary;
-        private long otherVocabulary;
-        private long concept;
-        private long collection;
-
-        public CategoriesCountDTO() {
-            this.terminologicalVocabulary = 0;
-            this.otherVocabulary = 0;
-            this.concept = 0;
-            this.collection = 0;
-        }
-
-        public CategoriesCountDTO(Map<String, Long> categories) {
-            this.terminologicalVocabulary = categories.getOrDefault(
-                    "TerminologicalVocabulary", 0L);
-            this.otherVocabulary = categories.getOrDefault(
-                    "OtherVocabulary", 0L);
-            this.concept = categories.getOrDefault(
-                    "Concept", 0L);
-            this.collection = categories.getOrDefault("Collection", 0L);
-        }
-
-        public long getTerminologicalVocabulary() {
-            return terminologicalVocabulary;
-        }
-
-        public void setTerminologicalVocabulary(int terminologicalVocabulary) {
-            this.terminologicalVocabulary = terminologicalVocabulary;
-        }
-
-        public long getOtherVocabulary() {
-            return otherVocabulary;
-        }
-
-        public void setOtherVocabulary(int otherVocabulary) {
-            this.otherVocabulary = otherVocabulary;
-        }
-
-        public long getConcept() {
-            return concept;
-        }
-
-        public void setConcept(int concept) {
-            this.concept = concept;
-        }
-
-        public long getCollection() {
-            return collection;
-        }
-
-        public void setCollection(long collection) {
-            this.collection = collection;
-        }
-    }
-    */
-
     private Map<String, Long> categories;
     private Map<String, Long> statuses;
     private Map<String, Long> groups;
