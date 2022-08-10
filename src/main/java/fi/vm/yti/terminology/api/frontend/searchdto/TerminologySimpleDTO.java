9d0deb2d131ae3ad85e8fb0c7e073653084ad99e
topirissanen-atoz <topi.rissanen@atoz.fi>
2022-08-10 11:00:21 +0300

    Terminology type added to search results

--- src/main/java/fi/vm/yti/terminology/api/frontend/searchdto/TerminologySimpleDTO.java
+++ src/main/java/fi/vm/yti/terminology/api/frontend/searchdto/TerminologySimpleDTO.java
@@ -8,17 +8,20 @@ public class TerminologySimpleDTO {
     private String code;
     private String uri;
     private String status;
+    private String type;
     private Map<String, String> label;
 
     public TerminologySimpleDTO(final String id,
                                 final String code,
                                 final String uri,
                                 final String status,
+                                final String type,
                                 final Map<String, String> label) {
         this.id = id;
         this.code = code;
         this.uri = uri;
         this.status = status;
+        this.type = type;
         this.label = label;
     }
 
@@ -52,6 +55,14 @@ public class TerminologySimpleDTO {
 
     public void setStatus(final String status) {
         this.status = status;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(final String type) {
+        this.type = type;
     }
 
     public Map<String, String> getLabel() {
