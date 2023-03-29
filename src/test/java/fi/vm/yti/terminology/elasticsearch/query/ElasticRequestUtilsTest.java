129ef53bcb92c8c47e963f000f98f342e770ddb4
Tadej Šivak <tadej.sivak@gora.si>
2023-03-29 11:46:16 +0200

    keycloak dependency removed

--- src/test/java/fi/vm/yti/terminology/elasticsearch/query/ElasticRequestUtilsTest.java
+++ src/test/java/fi/vm/yti/terminology/elasticsearch/query/ElasticRequestUtilsTest.java
@@ -1,7 +1,7 @@
 package fi.vm.yti.terminology.elasticsearch.query;
 
 import fi.vm.yti.terminology.api.util.ElasticRequestUtils;
-import org.elasticsearch.index.query.QueryStringQueryBuilder;;
+import org.elasticsearch.index.query.QueryStringQueryBuilder;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -10,12 +10,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class ElasticRequestUtilsTest {
 
     @ParameterizedTest
-    @ValueSource(strings = {"+", "-", "=", "&", "|", "!", "(", ")", "{", "}", "[", "]", "^", "\"", "~", "*", "?", ":", "\\", "/"})
-    void testBuildSpecialCharacters(String prefix){
+    @ValueSource(strings = { "+", "-", "=", "&", "|", "!", "(", ")", "{", "}", "[", "]", "^", "\"", "~", "*", "?", ":",
+            "\\", "/" })
+    void testBuildSpecialCharacters(String prefix) {
         QueryStringQueryBuilder request = ElasticRequestUtils.buildPrefixSuffixQuery(prefix);
         // \prefix \prefix* *\prefix
         assertEquals("\\" + prefix + " \\" + prefix + "* *\\" + prefix, request.queryString());
     }
-
 
 }
